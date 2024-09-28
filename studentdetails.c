c9f6c8001c18112f991c874cb9aa64489a7ef9da
HithaHarish-csbmsce <hithaharish.cs23@bmsce.ac.in>
2024-09-28 21:55:21 +0530

    Create studentdetails.c
    A program in C that is used to collect the details of students and store them.

---
+++ studentdetails.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+int i,size=1;
+struct Student{
+    char USN[10];
+    char Name[40];
+    int Number[10];
+};
+struct Student student[50];
+printf("\n Enter the number of students:");
+scanf("%d",&size);
+printf("\nWELCOME TO STUDENT REGISTARTION PORTAL");
+for (i=0;i<size;i++)
+{
+    printf("\nSTUDENT: %d",i+1);
+    printf("\nEnter your 10 digit USN");
+    scanf("%s",student[i].USN);
+    printf("Enter your Name");
+    getchar();
+    gets(student[i].Name);
+    printf("Enter your 10 digit Mobile Number");
+    scanf("%d",&student[i].Number);
+}
+}
