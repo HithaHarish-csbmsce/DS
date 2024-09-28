3cbc52d0e12ec76d74abf983cdc1689c329a39ca
HithaHarish-csbmsce <hithaharish.cs23@bmsce.ac.in>
2024-09-28 22:09:55 +0530

    moveZeroes.C

--- moveZeroes.C
+++ moveZeroes.C
@@ -1,13 +1,16 @@
 void moveZeroes(int* nums, int numsSize) {
+// Defining 2 varibles i,j for the for loop
     int i,j=0;
     for (i=0;i<numsSize;i++)
     {
+        //if nums[i] is a non-zero element, move it to the start of an array.
         if (nums[i]!=0)
         {
             nums[j]=nums[i];
             j++;
         }
     }
+    //assigning 0 to the remaining indexes
     for (i=j;i<numsSize;i++)
     {
         nums[i]=0;
