6a05f6e379add1b6f9462cc0c481b809a1ad169f
Hitha Harish <hithaharish.cs23@bmsce.ac.in>
2025-01-01 23:59:28 +0530

    Create BST.C

---
+++ BST.C
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node* root, int data) {
+    if (root == NULL) return createNode(data);
+    if (data < root->data) root->left = insert(root->left, data);
+    else if (data > root->data) root->right = insert(root->right, data);
+    return root;
+}
+
+Node* findMin(Node* root) {
+    while (root && root->left != NULL) root = root->left;
+    return root;
+}
+
+Node* delete(Node* root, int data) {
+    if (root == NULL) return root;
+    if (data < root->data) root->left = delete(root->left, data);
+    else if (data > root->data) root->right = delete(root->right, data);
+    else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        Node* temp = findMin(root->right);
+        root->data = temp->data;
+        root->right = delete(root->right, temp->data);
+    }
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(Node* root) {
+    if (root) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(Node* root) {
+    if (root) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+    printf("\n");
+
+    printf("Preorder Traversal: ");
+    preorder(root);
+    printf("\n");
+
+    printf("Postorder Traversal: ");
+    postorder(root);
+    printf("\n");
+
+    root = delete(root, 50);
+
+    printf("Inorder After Deletion: ");
+    inorder(root);
+    printf("\n");
+
+    return 0;
+}
+OUTPUT:
+Inorder Traversal: 20 30 40 50 60 70 80
+Preorder Traversal: 50 30 20 40 70 60 80
+Postorder Traversal: 20 40 30 60 80 70 50
+Inorder After Deletion: 20 30 40 60 70 80
